564bb9208648a86045bbe57f478541463c75a061
Jungeun Kwon <wow_v_v@naver.com>
2020-01-08 19:07:07 +0900

    계단 오르기 - dp 이용(2020.01.08)

---
+++ BaekJoon/src/Sol_2579_BJ.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Sol_2579_BJ {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int arr[] = new int[N+1];
+		int dp[][] = new int[2][N+1];
+		for(int i = 1; i <= N; i ++)
+		{
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		dp[1][1] = arr[1];
+		dp[0][1] = arr[1];
+		dp[0][2] = arr[2];
+		dp[1][2] = dp[0][1] + arr[2];
+		for(int i = 2; i <= N ; i ++)
+		{
+			dp[0][i] = Math.max(dp[0][i-2]+arr[i], dp[1][i-2]+arr[i]);
+			dp[1][i] = dp[0][i-1] + arr[i];
+		}
+		for(int i = 0; i < 2 ; i ++)
+		{
+			for(int j = 0; j <= N; j ++)
+			{
+				System.out.print(dp[i][j] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println(Math.max(dp[0][N], dp[1][N]));
+			
+	}
+
+}
