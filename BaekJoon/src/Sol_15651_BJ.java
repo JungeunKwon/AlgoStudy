a68f66027cb097a8db7f23b977f1c5e88021d4f5
JungeunKwon <wow_v_v@naver.com>
2020-08-16 04:47:45 +0900

    N과 M (3) - 순열

---
+++ BaekJoon/src/Sol_15651_BJ.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class Sol_15651_BJ {
+	public static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		boolean flag[] = new boolean[N];
+		int arr[] = new int[M];
+		Combi(N, M, 0, flag, arr);
+		System.out.println(sb.toString());
+	}
+
+	private static void Combi(int n, int m, int idx, boolean[] flag, int[] arr) {
+		if(idx >= m )
+		{
+			for(int i = 0; i  < m; i ++)
+			{
+				sb.append(arr[i]+1 + " ");
+			}
+			sb.append("\n");
+			return;
+		}
+		for(int j = 0; j < n ;j ++)
+		{
+			
+			arr[idx] = j;
+			Combi(n, m, idx + 1, flag, arr);
+
+		}
+	}
+
+
+	
+
+}
