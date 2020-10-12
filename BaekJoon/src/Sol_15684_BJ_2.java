f6555462aba941d985698ba63999c9ab70da619e
JungeunKwon <wow_v_v@naver.com>
2020-10-12 20:29:16 +0900

    사다리 조작 - 구현 (2020.10.12)

--- BaekJoon/src/Sol_15684_BJ_2.java
+++ BaekJoon/src/Sol_15684_BJ_2.java
@@ -8,7 +8,7 @@ public class Sol_15684_BJ_2 {
 	public static int M;
 	public static boolean arr[][];
 	public static boolean flag;
-
+ 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
